b99caf57171b7c9bed3b586db1026b0b3e69d56a
Mauricio <maubot01@gmail.com>
2021-02-04 17:40:42 -0400

    deleting console.log

--- src/components/Button/Button.component.js
+++ src/components/Button/Button.component.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Btn } from "./Button.styles";
 
 export const Button = ({ imp, text, link }) => {
-  console.log(imp);
   return (
     <Btn imp={imp}>
       <a href={link}>{text}</a>
