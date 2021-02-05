71aca9fd804ea1eb85c027edb6262603c03d7d69
Mauricio <maubot01@gmail.com>
2021-02-05 08:23:26 -0400

    function for responsive design

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,36 @@
 export const breakpoints = {
-  mobile: "520px",
+  mobile: "375px",
+  tablet: "768px",
+  desktop: "1200px",
+  large: "1400px",
+};
+
+export const device = {
+  mobile: (styles) => {
+    return `@media (min-width:${breakpoints.mobile}){
+      ${styles}
+    }`;
+  },
+  tablet: (styles) => {
+    return `@media (min-width:${breakpoints.tablet}){
+      ${styles}
+    }`;
+  },
+  desktop: (styles) => {
+    return `@media (min-width:${breakpoints.desktop}){
+      ${styles}
+    }`;
+  },
+  large: (styles) => {
+    return `@media (min-width:${breakpoints.large}){
+      ${styles}
+    }`;
+  },
+  custom: (styles, width) => {
+    return `@media (min-width:${width}){
+      ${styles}
+    }`;
+  },
 };
 
 export const colors = {
